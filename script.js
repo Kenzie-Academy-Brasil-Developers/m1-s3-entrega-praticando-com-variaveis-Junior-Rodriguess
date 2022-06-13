0d18652c96b9e35f80a2c801c183cc4151994c40
Junior Rodrigues <juniorpsicorodrigues@gmail.com>
2022-06-13 15:35:09 -0300

    Atividade variavel

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome = "Junior"
+let sobrenome = "Rodrigues"
+let anoNascimento = 1997
+let cidade = "Florianópolis-SC"
+
+console.log(`Me chamo ${nome} ${sobrenome} e tenho ${2022-anoNascimento}. 
+Atualmente Moro em ${cidade}.`);
